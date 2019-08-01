5d1d2b57aeaea36122fa979ea754c927ded3f99a
Joe Previte <joe@echobind.com>
2019-08-01 14:55:52 -0700

    feat: add basic sign up to auth controller

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -10,4 +10,9 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,13 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User({ username, password });
+    await user.save();
+  }
+}

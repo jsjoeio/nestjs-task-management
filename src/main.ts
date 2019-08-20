0b4281f3c92aadd3355bdaf32197e3da62a09a02
Joe Previte <joe@echobind.com>
2019-08-20 15:35:49 -0700

    feat: set up final configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
-
   const app = await NestFactory.create(AppModule);
-  const PORT = 3000;
+  const origin = serverConfig.origin;
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin });
+    logger.log(`Accepting requests from origin "${origin}`);
+  }
+
+  const PORT = process.env.PORT || serverConfig.port;
   await app.listen(PORT);
   logger.log(`Application listening on port: ${PORT}`);
 }
